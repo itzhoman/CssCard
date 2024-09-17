e152116e3d75c95cc6ba4e369cd40983b8125f8d
Hooman <psnforhoman@gmail.com>
2024-09-17 13:25:13 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.card-btn');
+const card = document.querySelector('.card');
+
+
+
+button.addEventListener('click', () => {
+    card.classList.toggle('change');
+});
